44e2866ad04e5973a8664fb604b6cc044097ae0f
xWass <spectatorgaming02@gmail.com>
2022-07-19 21:16:05 -0400

    updated for embeds will add exceptions later

--- src/commands/playing.js
+++ src/commands/playing.js
@@ -1,19 +1,32 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
-const { EmbedBuilder, GuildMember } = require("discord.js");
-const { Player, QueryType, QueueRepeatMode } = require("discord-player");
+const {SlashCommandBuilder}=require("@discordjs/builders");
+const {EmbedBuilder, GuildMember}=require("discord.js");
+const {Player, QueryType, QueueRepeatMode}=require("discord-player");
 
 
-module.exports = {
+module.exports={
   data: new SlashCommandBuilder()
-  .setName("playing")
-  .setDescription("Current playing song!"),
+    .setName("playing")
+    .setDescription("Current playing song!"),
   run: async (interaction) => {
-    const player = interaction.client.player
-    const queue = player.getQueue(interaction.guildId);
-    if(queue.playing){
-      interaction.reply(`The song playing is ${ queue.current.title } by ${ queue.current.author }`)
+    const player=interaction.client.player;
+    const queue=player.getQueue(interaction.guildId);
+    if (queue.playing) {
+      interaction.reply({
+        embeds: [{
+          title: `Now playing: ${ queue.current.title } by ${ queue.current.author }`,
+          footer: {
+            text: `Requested by ${ queue.current.requestedBy.tag }`
+          },
+          color: 'RANDOM',
+        }]
+      });
     } else {
-      interaction.reply("No song is playing")
+      interaction.reply({
+        embeds: [{
+          title: `Nothing is playing.`,
+          color: 'RANDOM',
+        }]
+      });
     }
   }
-}
+};
