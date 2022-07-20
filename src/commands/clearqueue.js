65c0a8ab4ca8c9efcc4344512e4a9be20c2f31cf
xWass <73602180+xWass@users.noreply.github.com>
2022-07-19 22:09:34 -0400

    fucking

--- src/commands/clearqueue.js
+++ src/commands/clearqueue.js
@@ -1,24 +1,37 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
-const { EmbedBuilder, GuildMember } = require("discord.js");
-const { Player, QueryType, QueueRepeatMode } = require("discord-player");
+const {SlashCommandBuilder}=require("@discordjs/builders");
+const {EmbedBuilder, GuildMember}=require("discord.js");
+const {Player, QueryType, QueueRepeatMode}=require("discord-player");
 
 
-module.exports = {
+module.exports={
   data: new SlashCommandBuilder()
   .setName("clear")
   .setDescription("Clear the queue!"),
   run: async (interaction) => {
-    const player = interaction.client.player
-    const queue = player.getQueue(interaction.guildId);
-        if (!queue || !queue.playing) return void interaction.reply({ content: "There are no songs playing" });
-        const currentTrack = queue.current;
-        if(!queue.metadata) return;
-       queue.metadata.send({
-       content: `🎶 | The queue has been cleared!\n🙆 | Cleared by ${interaction.user.tag}`,
-       })
-    queue.tracks = []
-        return interaction.reply({
-            content: `I have cleared the queue!`
-        });
+    const player=interaction.client.player;
+    const queue=player.getQueue(interaction.guildId);
+    if (!queue||!queue.playing) return void interaction.reply({content: "There are no songs playing"});
+    const currentTrack=queue.current;
+    const success=queue.skip();
+    if (!queue.metadata) return;
+    queue.metadata.send({
+      embeds: [{
+        title: `Queue has been cleared by ${ interaction.user.tag }`,
+        footer: {
+          text: `Requested by ${ currentTrack.requestedBy.tag }`
+        },
+        color: '000001',
+      }],
+    });
+    queue.tracks=[];
+    return void interaction.reply({
+      // content: success? `Current song skipped!`:"Something went wrong!",
+      embeds: success? [{
+        title: `Queue has been cleared by ${ interaction.user.tag }`,
+      }]:
+        [{
+          title: `Something went wrong!`,
+        }]
+    });
   }
-}
+};
