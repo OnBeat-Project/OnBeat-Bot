676e0b4ef5cbb717fb2f314af8605be4e4ca9630
Riley <87399106+rileyscool@users.noreply.github.com>
2022-07-19 18:04:00 -0700

    Update playing.js

--- src/commands/playing.js
+++ src/commands/playing.js
@@ -10,7 +10,7 @@ module.exports = {
   run: async (interaction) => {
     const player = interaction.client.player
     const queue = player.getQueue(interaction.guildId);
-    if(!queue || !queue.playing){
+    if(!queue.current){
   interaction.reply("Nothing is currently playing!")
     }else{
 interaction.reply(`The song playing is ${queue.current.title} by ${queue.current.author}`)
