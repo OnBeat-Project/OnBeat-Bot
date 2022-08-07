c9c4bb5ffe7b7329b828176a1e0dfaebee967f8a
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2022-08-06 22:42:21 -0300

    Delete src/.old.website directory

--- src/.old.website/Routers/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  // console.log(req);
-  res.render("index.ejs", {req,res})
-})
-
-module.exports = router;
